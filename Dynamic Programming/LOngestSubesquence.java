8ced78b2cf2a36f578cc9d33601d44646d569d21
Naveen Reddy <53139207+NaveenKumarReddyK@users.noreply.github.com>
2020-10-09 21:46:23 +0530

    Update LOngestSubesquence.java

--- Dynamic Programming/LOngestSubesquence.java
+++ Dynamic Programming/LOngestSubesquence.java
@@ -5,21 +5,21 @@ public class LOngestSubesquence {
         //If not equal max(left,top)
         char[] arr1 = s1.toCharArray();
         char[] arr2 = s2.toCharArray();
-        int[][] arr = new int[arr1.length+1][arr2.length+1] ;
+        int[][] dp = new int[arr1.length+1][arr2.length+1] ;
         for(int i=0;i<arr1.length;i++){
             for(int j=0;j<arr2.length;j++){
                 if(i == 0 || j == 0){
-                    arr[i][j] = 0;
+                    dp[i][j] = 0;
                 }
                 else if(arr1[i-1] == arr2[j-1]){
-                    arr[i][j] = arr[i-1][j-1];
+                    dp[i][j] = dp[i-1][j-1];
                 }
                 else {
-                    arr[i][j] = Math.max(arr[i-1][j],arr[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        System.out.println(arr[arr1.length][arr2.length]);
+        System.out.println(dp[arr1.length][arr2.length]);
     }
     public static void main(String[] args){
         String s1 = "NAVEEN";
