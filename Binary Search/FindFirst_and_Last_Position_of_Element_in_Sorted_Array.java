8bdb02a7d51c6ee7e115a78daf8c858dadb83eeb
Naveen Reddy <53139207+NaveenKumarReddyK@users.noreply.github.com>
2020-10-10 19:10:32 +0530

    Update FindFirst_and_Last_Position_of_Element_in_Sorted_Array.java

--- Binary Search/FindFirst_and_Last_Position_of_Element_in_Sorted_Array.java
+++ Binary Search/FindFirst_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -1,3 +1,7 @@
+// Main Idea: 
+// To get left position : we need to check the mid value with "HIGH" i.e, nums[mid] >= target then high = mid-1
+// To get right positoin : we need to check mid value with "LOW" i.e, nums[mid] <= target then low = mid + 1
+
 class FindFirst_and_Last_Position_of_Element_in_Sorted_Array {
     public int[] searchRange(int[] nums, int target) {
         //finding the first postion --> normal binary seach
@@ -14,15 +18,17 @@ class FindFirst_and_Last_Position_of_Element_in_Sorted_Array {
         while(low <= high){
             int mid = (low + high) >>> 1;
             if(isEnd){
+                //To get leftMost value
                 // > to check if there are any elements to the right of low
-                if(arr[mid] > target){
-                   high = mid -1;
+                if(arr[mid] <= target){
+                   low = mid + 1;
                 }
                 else{
-                    low = mid +1;
+                    hgih = mid - 1;
                 }
             }
             else{
+                //To get right most value
                 // >=  to check if there are any elements to the left of hgih
                 if(arr[mid] >= target){
                     high = mid - 1;
