efb1e0cb742cd5fd931f5c8e1d253c0dcbcaec40
Naveen Reddy <53139207+NaveenKumarReddyK@users.noreply.github.com>
2020-10-11 10:29:18 +0530

    Create Sum_Root_to_Leaf_Numbers.java

---
+++ Binary Search Tree/Sum_Root_to_Leaf_Numbers.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root,0);
+    }
+    public int dfs(TreeNode root,int sum){
+        if(root == null){
+            return 0;
+        }
+        int currSum = sum*10 + root.val;
+        if(root.left == null && root.right == null){
+            return currSum;
+        }
+        int leftSum = dfs(root.left,currSum);
+        int rightSum = dfs(root.right,currSum);
+        return leftSum + rightSum;
+    }
+}
