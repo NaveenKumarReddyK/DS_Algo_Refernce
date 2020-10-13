42213b2e3838eab692875f497025f029973e924e
Naveen Reddy <53139207+NaveenKumarReddyK@users.noreply.github.com>
2020-10-13 08:45:47 +0530

    Update Integer_To_Roman.java

--- Arrays/Integer_To_Roman.java
+++ Arrays/Integer_To_Roman.java
@@ -1,20 +1,38 @@
+//Link : https://leetcode.com/problems/integer-to-roman/
 
 import java.util.*;
-//Integer to Roman for number below 4000
 
-public class Integer_To_Roman {
-    public static String getRomanNumber(int num) {
-            String M[] = {"", "M", "MM", "MMM"};
-            String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-            String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-            String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-            
-            return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+class Integer_to_Roman_General{ 
+        //get all the numbers from 1 to 1000 where the changes occurs (Eg. 40 => XL )
+        static int[] divStore = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        static String[] romanStore  = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    public static String intToRoman(int num){
+       
+        //first get the leading number multiple and recursively subtract and add it to result
+        //recusrsively call numbers
+        return getRoman(num);
+    }
+    public static String getRoman(int num){
+        //base case
+        if(num == 0){
+            return "";
+        }
+        int index = getDiv(num);
+        return romanStore[index] + getRoman(num - divStore[index]);
+    }
+    public static int getDiv(int num){
+        //traverse through numstore
+        for(int i=0;i<divStore.length;i++){
+                if(num/divStore[i] != 0){
+                    return i;
+                }
+        }
+        return -1;
     }
     public static void main(String[] args) {
-      
         Scanner sc = new Scanner(System.in);
-        int num  = sc.nextInt();
-        System.out.println(getRomanNumber(num));
+        int num = sc.nextInt();
+        System.out.println(intToRoman(num));
+        sc.close();
     }
 }
