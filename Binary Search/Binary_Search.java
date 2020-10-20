689f1f8d07ee1982cc1b02c49a2d99f0726d8ee5
Naveen Reddy <53139207+NaveenKumarReddyK@users.noreply.github.com>
2020-10-20 17:12:26 +0530

    added left search and right search

--- Binary Search/Binary_Search.java
+++ Binary Search/Binary_Search.java
@@ -1,5 +1,6 @@
+// Basic search with resulting value to be position of target itself
 class Binary_Search {
-    public int search(int[] nums, int target) {
+    public int basic_search(int[] nums, int target) {
         int low = 0;
         int high = nums.length-1;
         while(low <= high){
@@ -17,5 +18,43 @@ class Binary_Search {
         }
         return -1;
         
+    }
+
+// Finding the first number to the left of the given target
+public int left_search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        int index = -1;
+        while(low <= high){
+            // int mid = low + (high - low)/2;
+            int mid = (low + high) >>> 1;
+            if(nums[mid] >= target){
+                high = mid-1;
+            }
+            else{
+                index = mid;
+                low = mid+1;
+            }
+        }
+        return index;
+    }
+   
+// Finding the first number to the left of the given target
+public int right_search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        int index = -1;
+        while(low <= high){
+            // int mid = low + (high - low)/2;
+            int mid = (low + high) >>> 1;
+            if(nums[mid] <= target){
+                low = mid+1;
+            }
+            else{
+                index = mid;
+                high = mid-1;
+            }
+        }
+        return index;
     }
 }
