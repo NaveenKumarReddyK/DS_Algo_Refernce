48cc145008497899b7108e2cf7fc9a40de4308e5
Naveen Reddy <53139207+NaveenKumarReddyK@users.noreply.github.com>
2020-10-20 19:21:56 +0530

    Update Shortest_Common_String.java

--- Strings/Shortest_Common_String.java
+++ Strings/Shortest_Common_String.java
@@ -1,70 +1,70 @@
-//import java.util.*;
-//
-////Description
-//// Find shortest string made from String1 and String2 such that it consists of both String1 and String2
-//class Shortest_Common_String {
-//    public static String shortestCommonSupersequence(String str1, String str2) {
-//        // find lcs of str1 and str2
-//        String lcs = longestCommomSubString(str1, str2);
-//        StringBuilder res = new StringBuilder();
-//        int i = 0;
-//        int j = 0;
-//        // traverse through LCS
-//        for (int k = 0; k < lcs.length(); k++) {
-//            // check if s1 is not traversed to end
-//            // and curr char is not equal to the one int the lcs
-//            while (i < str1.length() && str1.charAt(i) != lcs.charAt(k)) {
-//                res.append(str1.charAt(i));
-//                i++;
-//            }
-//
-//            // same here but in s2
-//            while (j < str2.length() && str2.charAt(j) != lcs.charAt(k)) {
-//                res.append(str2.charAt(j));
-//                j++;
-//            }
-//
-//            // if completed traversal of above two loops
-//            // add curr char in the lcs string
-//
-//            res.append(lcs.charAt(k));
-//            i++;
-//            j++;
-//        }
-//
-//        // append remaining substrings in s1 and s2 to response
-//        res.append(str1.substring(i)).append(str2.substring(j));
-//
-//        // output response string
-//        return res.toString();
-//
-//    }
-//
-//    public static String longestCommomSubString(String s1, String s2) {
-//        int len1 = s1.length();
-//        int len2 = s2.length();
-//        String[][] dp = new String[len1 + 1][len2 + 1];
-//        for (int i = 0; i < dp.length; i++) {
-//            Arrays.fill(dp[i], "");
-//        }
-//        for (int i = 1; i <= len1; i++) {
-//            for (int j = 1; j <= len2; j++) {
-//                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-//                    dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i - 1);
-//                } else {
-//                    dp[i][j] = (dp[i - 1][j].length() > dp[i][j - 1].length()) ? dp[i - 1][j] : dp[i][j - 1];
-//                }
-//            }
-//        }
-//
-//        return dp[len1][len2];
-//    }
-//
-//    public static void main(String[] args){
-//        Scanner sc = new Scanner(System.in);
-//        Stirng input1 = sc.next();
-//        String input2 = sc,next();
-//        System.out.println(shortestCommonSupersequence(input1,input2));
-//        sc.close();
-//    }
-//}
+// Link : https://leetcode.com/problems/shortest-common-supersequence/
+
+import java.util.*;
+
+class Shortest_Common_String {
+   public static String shortestCommonSupersequence(String str1, String str2) {
+       // find lcs of str1 and str2
+       String lcs = longestCommomSubString(str1, str2);
+       StringBuilder res = new StringBuilder();
+       int i = 0;
+       int j = 0;
+       // traverse through LCS
+       for (int k = 0; k < lcs.length(); k++) {
+           // check if s1 is not traversed to end
+           // and curr char is not equal to the one int the lcs
+           while (i < str1.length() && str1.charAt(i) != lcs.charAt(k)) {
+               res.append(str1.charAt(i));
+               i++;
+           }
+
+           // same here but in s2
+           while (j < str2.length() && str2.charAt(j) != lcs.charAt(k)) {
+               res.append(str2.charAt(j));
+               j++;
+           }
+
+           // if completed traversal of above two loops
+           // add curr char in the lcs string
+
+           res.append(lcs.charAt(k));
+           i++;
+           j++;
+       }
+
+       // append remaining substrings in s1 and s2 to response
+       res.append(str1.substring(i)).append(str2.substring(j));
+
+       // output response string
+       return res.toString();
+
+   }
+
+   public static String longestCommomSubString(String s1, String s2) {
+       int len1 = s1.length();
+       int len2 = s2.length();
+       String[][] dp = new String[len1 + 1][len2 + 1];
+       for (int i = 0; i < dp.length; i++) {
+           Arrays.fill(dp[i], "");
+       }
+       for (int i = 1; i <= len1; i++) {
+           for (int j = 1; j <= len2; j++) {
+               if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                   dp[i][j] = dp[i - 1][j - 1] + s1.charAt(i - 1);
+               } else {
+                   dp[i][j] = (dp[i - 1][j].length() > dp[i][j - 1].length()) ? dp[i - 1][j] : dp[i][j - 1];
+               }
+           }
+       }
+
+       return dp[len1][len2];
+   }
+
+   public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       String input1 = sc.next();
+       String input2 = sc.next();
+       System.out.println(shortestCommonSupersequence(input1,input2));
+       sc.close();
+   }
+}
