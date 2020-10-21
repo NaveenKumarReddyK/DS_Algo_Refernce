17fd79aeb1c5d8a8fddf75ac06e6a843d085d3c0
Naveen Reddy <53139207+NaveenKumarReddyK@users.noreply.github.com>
2020-10-21 10:33:01 +0530

    Update Does_Node_Exist_Complete_Tree.java

--- Binary Search Tree/Does_Node_Exist_Complete_Tree.java
+++ Binary Search Tree/Does_Node_Exist_Complete_Tree.java
@@ -6,6 +6,7 @@
     TreeNode(int x) { val = x; }
 }
 public class Solution {
+ //SOLUTION 1 :
     //to get the position of the node we need to get the binary representaion of the number 
     public static boolean nodeExists(TreeNode root,int position ){
         //get the binary representatio of the position
@@ -24,12 +25,34 @@ public class Solution {
         }
         return root != null;
     }
+ //SOLUTION 2 : 
+ //to get the position of the node we need to get the binary representaion of the number 
+    //divde the number by 2 and store them in stack to traverse to the position in tree
+    public static boolean doesNodeExists(TreeNode root,int position){
+        //get the divided values in a stack
+        Stack<Integer> stack = new Stack<>();
+        while(position != 1){
+            stack.push(position);
+            position = position/2;
+        }
+        //traverse through stack and is num leaves rem 0 when divides by 2 => go left || go right
+        while(!stack.isEmpty()){
+            root = (stack.pop() %2 == 0) ? root.left : root.right;
+            if(root == null){
+                return false;
+            }
+        }
+        return root != null;
+        
+    }
     public static void main(String[] args){
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.left.left = new TreeNode(4);
         root.right = new TreeNode(3);
         root.right.left = new TreeNode(6);
+     
         System.out.println(nodeExists(root,6));
+       System.out.println(doesNodeExists(root, 5));
     }
 }
